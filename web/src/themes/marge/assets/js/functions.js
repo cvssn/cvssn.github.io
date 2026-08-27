3f4aa0a873cfaa134944b4987828cf307a0ac951
cavassani <lcs.cvssn.qrz@gmail.com>
2026-08-27 01:50:14 -0300

    adição de scripts essenciais para o `elementor-pro` e funções de temas

---
+++ web/src/themes/marge/assets/js/functions.js
@@ -0,0 +1,7 @@
+/* =========================================================
+   jQuery / jQuery UI (NO-CONFLICT SAFE)
+   Only put code in here if you need $ or jQuery plugins
+========================================================= */
+jQuery(function ($) {
+
+});
